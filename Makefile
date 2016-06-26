ae26112ad51515ca8f93382ae7746b22380079b1
Jack Dwyer <jackjack.dwyer@gmail.com>
2016-06-26 14:08:21 -0400

    include total distance

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+.PHONY: assets
+
 years:
 	./generate_maps_filter_year.py
 	cp /tmp/*_index.html /home/jdwyer/programming/yearly_biking/
@@ -5,3 +7,5 @@ years:
 	aws s3 cp --acl public-read /home/jdwyer/programming/yearly_biking/index.html s3://cycling.jackdwyer.org/ --profile personal
 	aws s3 cp --acl public-read /home/jdwyer/programming/yearly_biking/2015_index.html s3://cycling.jackdwyer.org/ --profile personal
 
+assets:
+	aws s3 cp --acl public-read --recursive assets/ s3://cycling.jackdwyer.org/ --profile personal
--- generate_maps_filter_year.py
+++ generate_maps_filter_year.py
@@ -24,29 +24,40 @@ years = dict()
 for el in data:
     date = datetime.strptime(el['start_date_local'], '%Y-%m-%dT%H:%M:%SZ')
     try:
-        years[date.year] 
+        years[date.year]
+        years[date.year]['coords']
+        years[date.year]['distance']
     except KeyError:
-        years[date.year] = list()
+        years[date.year] = dict()
+        years[date.year]['coords'] = list()
+        years[date.year]['distance'] = list()
 
-    years[date.year].append(geojson(PolylineCodec().decode(el['map']['summary_polyline'])))
+    years[date.year]['distance'].append((el['distance']))
+    years[date.year]['coords'].append(geojson(PolylineCodec().decode(el['map']['summary_polyline'])))
 
 
 for year in years.keys():
     i = 0
     year_string = ""
     year_string += "var cyclelines = ["
-    end = len(years[year])
-    for el in years[year]:
+    end = len(years[year]['coords'])
+    for el in years[year]['coords']:
         year_string += json.dumps(el)
+        print(el)
+
         if not i == end-1:
             year_string += ","
         i += 1
+
     year_string += "];"
+
+    year_total = round(sum(years[year]['distance'])/1000, 2)
 
     template = env.get_template('index.html')
     with open("/tmp/{}_index.html".format(year), 'w') as f:
-        f.write(template.render(theLines=year_string))
+        f.write(template.render(theLines=year_string, distance=year_total))
 
     print("Wrote: {}".format(year))
+    print("Total Distance: {}".format(year_total))
 
 # pp.pprint(years)
